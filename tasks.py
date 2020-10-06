4740f2f78b539ea821ec41cd4ffab64e1702d159
dutt <ustyantsev.makar@mail.ru>
2020-10-06 23:28:27 +0500

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,8 +1,21 @@
-import module
+# import module
 
-command = input('Введите команду\n')
+# command = input('Введите команду\n')
 
-command = command.lower()
+# command = command.lower()
 
-if command == 'изменить базу':
-	changes = input('Что вы хотите изменить?').lower()
+# if command == 'изменить базу':
+# 	changes = input('Что вы хотите изменить?').lower()
+p = int(input('Введите число\n'))
+
+if p > 10:
+	exit(1)
+
+# while p <= 10:
+# 	if p % 2 == 0:
+# 		print(p)
+# 	p+=1
+
+for k in range(p+1, 11):
+	if k % 2 == 0:
+		print(k)
